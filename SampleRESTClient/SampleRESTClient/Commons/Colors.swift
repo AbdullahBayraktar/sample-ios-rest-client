2b24ae37365e66c9a3a5b5b9aa0c39e38813b8a3
Abdullah Bayraktar <abdullahbayraktar9@gmail.com>
2017-09-11 02:48:18 +0300

    Add Colors Class

---
+++ SampleRESTClient/SampleRESTClient/Commons/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  SampleRESTClient
+//
+//  Created by Abdullah Bayraktar on 11/09/2017.
+//  Copyright © 2017 AB. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    
+    struct Gray {
+        static let light = UIColor.lightGray
+        static let medium = UIColor(red:0.0/255.0, green:0.0/255.0, blue:0.0/255.0, alpha:0.5)
+        static let dark = UIColor.darkGray
+        static let thinLight = UIColor(red:170.0/255.0, green:170.0/255.0, blue:170.0/255.0, alpha:1.0)
+    }
+}
